32a2e1f5aab44dc7ade46943e2bb65c0646eceea
edem-wilfried-amouzou <edemwilfriedamouzou@gmail.com>
2026-06-04 11:24:41 +0000

    dernier modifs

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -88,22 +89,27 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
+from dotenv import load_dotenv
+load_dotenv()
+
+if os.environ.get('DATABASE_URL'):
+    # Pour Render ou Supabase en production
     DATABASES = {
         'default': dj_database_url.config(
-            default=DATABASE_URL,
+            default=os.environ.get('DATABASE_URL'),
             conn_max_age=600,
-            ssl_require=True  # Important pour les bases externes (ex: Supabase/Neon)
+            ssl_require=True # Supabase exige SSL
         )
     }
 else:
+    # Pour votre développement local (SQLite)
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
 
 
 # Password validation
