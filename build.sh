5ca4d94e66999c2c9fb4a1f59eb94b4e9a8c2e5b
edem-wilfried-amouzou <edemwilfriedamouzou@gmail.com>
2026-06-04 11:30:05 +0000

    dernier modifs

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 # Quitter en cas d'erreur
 set -o errexit
 
-# 1. Installer les dépendances Python
-pip install -r requirements.txt
+npm install && npm run build && pip install -r requirements.txt
 
-# 2. Installer les dépendances Node pour DaisyUI
-npm install
-
-# 3. Compiler le CSS (DaisyUI/Tailwind)
-# Assurez-vous que cette commande correspond à votre package.json
-npm run build 
 
 # 4. Collecter tous les fichiers statiques (CSS + VOS FICHIERS JS)
 # Whitenoise s'occupera de les servir efficacement
