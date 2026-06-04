605b694819623a09fd09e8c4c2b1a8fd843c0c9d
edem-wilfried-amouzou <edemwilfriedamouzou@gmail.com>
2026-06-04 12:01:54 +0000

    ajout de dependance daisyui2

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,16 +1,13 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-for-dev-only')
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
