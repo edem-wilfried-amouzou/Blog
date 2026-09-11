7ff0b44830589c331ada31957647813cdd6c315d
edem-wilfried-amouzou <edemwilfriedamouzou@gmail.com>
2026-09-11 07:36:30 +0000

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -40,12 +40,12 @@ if settings.DEBUG:
 
 urlpatterns = urlpatterns + [
     # Serve BlogIndexPage as homepage
-    # path("", serve_blog_index, name="blog_index_homepage"),
+    path("", serve_blog_index, name="blog_index_homepage"),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("", include(wagtail_urls)),
+    # path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
+    path("pages/", include(wagtail_urls)),
 ]
